be8a00559e0879bc7ee5d59f335bcdc283ba4a8c
javisolercanto <jsolercanto11@gmail.com>
2021-02-01 15:06:50 +0100

    readme update and email improvements

--- .github/actions/mail_action/index.js
+++ .github/actions/mail_action/index.js
@@ -4,20 +4,24 @@ const core = require("@actions/core");
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'javisolertests@gmail.com',
+    user: core.getInput('EMAIL_FROM'),
     pass: core.getInput('PASSWORD')
   }
 });
 
 const options = {
-  from: 'javisolertests@gmail.com',
-  to: core.getInput('EMAIL'),
+  from: core.getInput('EMAIL_FROM'),
+  to: core.getInput('EMAIL_TO'),
   subject: 'Resultado del workflow ejecutado',
-  text: `Se ha realizado un push en la rama githubActions_improvement que ha provocado la ejecución del workflow Bingo_Workflow con los siguientes resultados:
+  text: `Se ha realizado un push en ${core.getInput('REPO_NAME')} sobre la rama ${core.getInput('BRANCH_NAME')} que ha provocado la ejecución del workflow ${core.getInput('WORKFLOW_NAME')} con los siguientes resultados:
   - syntax_check_job: ${core.getInput('SYNTAX')}
   - test_execution_job: ${core.getInput('TEST')}
   - build_statics_job: ${core.getInput('BUILD')}
-  - deploy_job: ${core.getInput('DEPLOY')}`
+  - deploy_job: ${core.getInput('DEPLOY')}
+  
+  Acción realizada por el usuario: ${core.getInput('ACTOR')}
+  
+  Repositorio ${core.getInput('REPO_NAME')} perteneciente a ${core.getInput('REPO_OWNER')}`
 };
 
 transporter.sendMail(options, function(error, info){
